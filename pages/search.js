55deff6f55760ea6d9f3c1888ea9041755af5ff8
jakobniinja <jakobyaro127@hotmail.com>
2021-11-07 14:35:14 +0100

    impl search layout

--- pages/search.js
+++ pages/search.js
@@ -9,7 +9,24 @@ const Search = () => {
             </ButtonContainer>
 
         <InputContainer>
+        <FromtoIcons>
+            <Circle  src={"https://img.icons8.com/ios-filled/50/9CA3AF/filled-circle.png"}/>
+            <Line src={"https://img.icons8.com/ios/50/9CA3AF/vertical-line.png"}/>
+            <Square src={"https://img.icons8.com/windows/50/000000/square-full.png"}/>
+        </FromtoIcons>
+        <InputBoxes>
+
+        <Input placeholder="Ange Plats" ></Input>
+                <Input  placeholder="Till?"></Input>
+        </InputBoxes>
+        <PlusIcon src={"https://img.icons8.com/ios/50/000000/plus-math.png"} />
         </InputContainer>
+        <SavedPlaces>
+            <StarIcon src={"https://img.icons8.com/ios-filled/50/ffffff/star--v1.png"}/>
+            Sparade Platser
+        </SavedPlaces>
+        
+        
            {/* Input container */}
            {/* saved places  */}
            {/* confirm location */}
@@ -17,11 +34,12 @@ const Search = () => {
     )
 }
 
-export default Search
-const Wrapper = tw.div`
-bg-gray-200 h-screen
+export default Search;
 
-`;
+const Wrapper = tw.div`
+bg-gray-200 bg-white h-screen
+
+`
 
 const ButtonContainer = tw.div`
 bg-white px-4
@@ -33,6 +51,38 @@ h-12
 
 `
 const InputContainer = tw.div`
+bg-white flex  items-center px-4 mb-2
+`
+const FromtoIcons = tw.div`
+w-10 flex flex-col ml-2 items-center
+`
+const Circle =tw.img`
+h-2.5
+`
+const Line = tw.img`
+h-10
+`
 
 
+const Square = tw.img`
+h-3
+
+`
+const InputBoxes =tw.div`
+flex flex-col flex-1
+`
+const Input = tw.input`
+h-10 bg-gray-200 my-2 rounded-2 p-2 outline-none border-none
+
+`
+const PlusIcon = tw.img`
+ w-10 h-10 bg-gray-200 rounded-full ml-3
+
+` 
+const SavedPlaces = tw.div`
+flex items-center bg-white px-4 py-2
+
+`
+const StarIcon = tw.img`
+bg-gray-400 w-10 h-10 p-2 rounded-full mr-2
 `
